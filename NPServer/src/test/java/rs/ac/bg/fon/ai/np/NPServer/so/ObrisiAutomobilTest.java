e441642e8849410014b849258441b4fae6649672
kalubre23 <luka.zola@hotmail.com>
2024-04-06 01:47:13 +0200

    Testirana sistemska operacija ObrisiAutomobil

---
+++ NPServer/src/test/java/rs/ac/bg/fon/ai/np/NPServer/so/ObrisiAutomobilTest.java
@@ -0,0 +1,39 @@
+package rs.ac.bg.fon.ai.np.NPServer.so;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import rs.ac.bg.fon.ai.np.NPCommon.domain.Automobil;
+import rs.ac.bg.fon.ai.np.NPCommon.domain.Marka;
+import rs.ac.bg.fon.ai.np.NPCommon.domain.UoceniKvar;
+import rs.ac.bg.fon.ai.np.NPServer.logic.Controller;
+
+class ObrisiAutomobilTest {
+
+	@Test
+	void executeOperationTest() throws Exception {
+		Automobil auto = new Automobil();
+		auto.setTablice("KG555333");
+		
+		Controller controller = new Controller();
+		controller.obrisiAutomobil(auto);
+		
+		//controller.pronadjiAutomobile() funkcija pretrazuje automobile prema imenu i prezimenu vlasnika
+		//Ako joj posaljemo ovako inicijalizovan automobil, vratice sve automobile iz baze
+		Automobil a1 = new Automobil();
+		a1.setImePrezimeVlasnika("");
+	
+		List<Automobil> sviAutomobili = controller.pronadjiAutomobile(a1);
+		
+		assertFalse(sviAutomobili.contains(auto));
+	}
+
+}
