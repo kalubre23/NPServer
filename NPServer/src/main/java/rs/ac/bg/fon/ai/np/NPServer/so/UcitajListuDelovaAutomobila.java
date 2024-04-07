395926f97067857982f2077917673c41a4721c47
kalubre23 <luka.zola@hotmail.com>
2024-04-07 22:23:00 +0200

    Dokumentovane sistemske operacije za vracanje svih domenskih objekata

--- NPServer/src/main/java/rs/ac/bg/fon/ai/np/NPServer/so/UcitajListuDelovaAutomobila.java
+++ NPServer/src/main/java/rs/ac/bg/fon/ai/np/NPServer/so/UcitajListuDelovaAutomobila.java
@@ -12,21 +12,41 @@ import rs.ac.bg.fon.ai.np.NPCommon.domain.DeoAutomobila;
 import rs.ac.bg.fon.ai.np.NPCommon.domain.DomenskiObjekat;
 
 /**
+ * Predstavlja konkretnu sistemsku operaciju koja vraca sve delove automobila iz baze. Nasledjuje 
+ * opstu sistemsku operaciju.
  *
- * @author Asus
+ * @see AbstractSO
+ * @author Luka Obrenic
+ * @since 1.0.0
  */
 public class UcitajListuDelovaAutomobila extends AbstractSO {
-    
+	/**
+	 * Lista svih delova automobila.
+	 * @see DeoAutomobila
+	 */
     List<DeoAutomobila> deloviAutomobila;
 
+    /**
+     * Vraca listu svih delova automobila.
+     * @return deloviAutomobila vraceni iz baze, kao lista objekata domenske klase Automobil.
+     */
     public List<DeoAutomobila> getDeloviAutomobila() {
         return deloviAutomobila;
     }
     
+    /**
+	 * Neparametrizovani konstruktor koji inicijalizuje listu svih delova automobila.
+	 * @throws Exception ako dodje do greske
+	 */
     public UcitajListuDelovaAutomobila() throws Exception {
         deloviAutomobila = new ArrayList<>();
     }
     
+    /**
+     * Konkretna implementacija sistemske operacije vracanja svih delova automobila iz baze.
+     * 
+     * Poziva se metoda za vracanje svih objekata domenske klase u database brokeru.
+     */
     @SuppressWarnings("unchecked")
 	@Override
     protected void executeOperation(DomenskiObjekat object) throws Exception {
--- NPServer/src/main/java/rs/ac/bg/fon/ai/np/NPServer/so/UcitajListuMarki.java
+++ NPServer/src/main/java/rs/ac/bg/fon/ai/np/NPServer/so/UcitajListuMarki.java
@@ -7,25 +7,46 @@ package rs.ac.bg.fon.ai.np.NPServer.so;
 
 import java.util.List;
 
+
 import rs.ac.bg.fon.ai.np.NPCommon.domain.DomenskiObjekat;
 import rs.ac.bg.fon.ai.np.NPCommon.domain.Marka;
 
 /**
+ * Predstavlja konkretnu sistemsku operaciju koja vraca sve marke automobila iz baze. Nasledjuje 
+ * opstu sistemsku operaciju.
  *
- * @author Asus
+ * @see AbstractSO
+ * @author Luka Obrenic
+ * @since 1.0.0
  */
 public class UcitajListuMarki extends AbstractSO {
 
+	/**
+	 * Lista svih marki automobila.
+	 * @see Marka
+	 */
     List<Marka> listaMarki;
 
+    /**
+     * Vraca listu svih marki automobila.
+     * @return listaMarki vraceni iz baze, kao lista objekata domenske klase Marka.
+     */
     public List<Marka> getListaMarki() {
         return listaMarki;
     }
-
+    /**
+	 * Neparametrizovani konstruktor koji poziva konstruktor opste sistemske operacije koja je nasledjena.
+	 * @throws Exception ako dodje do greske pri izvrsavanju konstruktora nadklase
+	 */
     public UcitajListuMarki() throws Exception {
         super();
     }
 
+    /**
+     * Konkretna implementacija sistemske operacije vracanja svih marki automobila iz baze.
+     * 
+     * Poziva se metoda za vracanje svih objekata domenske klase u database brokeru.
+     */
     @SuppressWarnings("unchecked")
 	@Override
     protected void executeOperation(DomenskiObjekat object) throws Exception {
