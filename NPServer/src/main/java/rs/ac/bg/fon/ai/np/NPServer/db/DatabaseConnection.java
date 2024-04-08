cb101da5bec26fedfba2d9314b91ff5a7bff47b7
kalubre23 <luka.zola@hotmail.com>
2024-04-08 15:20:47 +0200

    Dodato ucitavanje iz json fajla
    Dodat dbconfig.json fajl sa parametrima za povezivanje sa bazom i dodata
    funkcija getConnection koja ucitava ove parametre

--- NPServer/src/main/java/rs/ac/bg/fon/ai/np/NPServer/db/DatabaseConnection.java
+++ NPServer/src/main/java/rs/ac/bg/fon/ai/np/NPServer/db/DatabaseConnection.java
@@ -4,13 +4,19 @@
  */
 package rs.ac.bg.fon.ai.np.NPServer.db;
 
+import java.io.FileReader;
+import java.io.IOException;
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+
 /**
- * Predstavlja klasu koja je zaduzena za pravljenje konekcija ka bazi i dodeljivanje tih konekcija
- * DatabaseBroker-u i sistemskim operacijama zbog commit-ovanja i rollback-ovanja.
+ * Predstavlja klasu koja je zaduzena za pravljenje konekcija ka bazi i
+ * dodeljivanje tih konekcija DatabaseBroker-u i sistemskim operacijama zbog
+ * commit-ovanja i rollback-ovanja.
  * 
  * Inicijalizuje fiksirani broj konekcija koji se implementira kao queue.
  * Konekcija se dodeljuje pomocu pop(), a vraca u queue pomocu push() metode.
@@ -23,80 +29,102 @@ public class DatabaseConnection {
 	/**
 	 * Lista konekcija kao lista tipa Connection.
 	 */
-    private List<Connection> connectionPool;
-    /**
-     * Atribut tipa DatabaseConnection za implementaciju singleton paterna.
-     */
-    private static DatabaseConnection instance;
-    
-    /**
-     * Neparametrizovani privatni konstruktor za implementaciju singleton paterna.
-     * 
-     * Inicijalizuje listu konekcija, inicijalizuje 10 konekcija i dodaje ih u tu listu.
-     * 
-     * @throws SQLException ukoliko dodje do greske pri inicijalizaciji konekcije ka bazi
-     */
-    private DatabaseConnection() throws SQLException {
-        connectionPool = new ArrayList<>();
-        for (int i = 0; i < 10; i++) {
-            try {
-                String url = "jdbc:mysql://localhost:3306/seminarskips";
-                String user = "root";
-                String password = "";
-                Connection connection = DriverManager.getConnection(url, user, password);
-                System.out.println("Konekcija sa bazom podataka uspesno uspostavljena!");
+	private List<Connection> connectionPool;
+	/**
+	 * Atribut tipa DatabaseConnection za implementaciju singleton paterna.
+	 */
+	private static DatabaseConnection instance;
 
-                //iskljucujemo automatsko potvrdjivanje transakcije nakon svakog upita
-                connection.setAutoCommit(false);
-                connectionPool.add(connection);
-            } catch (SQLException ex) {
-                System.out.println("Greska! Konekcija sa bazom nije uspesno uspostavljena!\n" + ex.getMessage());
-                ex.printStackTrace();
-                throw ex;
-            }
-        }
-    }
+	/**
+	 * Neparametrizovani privatni konstruktor za implementaciju singleton paterna.
+	 * 
+	 * Inicijalizuje listu konekcija, inicijalizuje 10 konekcija i dodaje ih u tu
+	 * listu.
+	 * 
+	 * @throws SQLException ukoliko dodje do greske pri inicijalizaciji konekcije ka
+	 * bazi
+	 */
+	private DatabaseConnection() throws Exception {
+		connectionPool = new ArrayList<>();
+		for (int i = 0; i < 10; i++) {
+			try {
+				Connection connection = getConnection();
+				connectionPool.add(connection);
+			} catch (Exception ex) {
+				System.out.println("Greska! Konekcija sa bazom nije uspesno uspostavljena!\n" + ex.getMessage());
+				ex.printStackTrace();
+				throw ex;
+			}
+		}
+	}
 
-    /**
-     * Staticka metoda za implementaciju singleton paterna.
-     * 
-     * Vraca jedinu instancu ove klase. Ukoliko je ona null prvo ce se inicijalizovati.
-     * 
-     * @return instanca ove klase
-     * @throws SQLException izuzetak koji baca ukoliko ga baci poziv konstruktora DatabaseConnection()
-     */
-    public static DatabaseConnection getInstance() throws SQLException {
-        if (instance == null) {
-            instance = new DatabaseConnection();
-        }
-        return instance;
-    }
+	/**
+	 * Staticka metoda za implementaciju singleton paterna.
+	 * 
+	 * Vraca jedinu instancu ove klase. Ukoliko je ona null prvo ce se
+	 * inicijalizovati.
+	 * 
+	 * @return instanca ove klase
+	 * @throws SQLException izuzetak koji baca ukoliko ga baci poziv konstruktora
+	 *                      DatabaseConnection()
+	 */
+	public static DatabaseConnection getInstance() throws Exception {
+		if (instance == null) {
+			instance = new DatabaseConnection();
+		}
+		return instance;
+	}
 
-    /**
-     * Implementacija queue metode push.
-     * 
-     * Dodaje se konekcija na kraj liste.
-     * 
-     * @param connection konekcija tipa Connection
-     */
-    public synchronized void push(Connection connection) {
-        connectionPool.add(connection);
-    }
+	/**
+	 * Implementacija queue metode push.
+	 * 
+	 * Dodaje se konekcija na kraj liste.
+	 * 
+	 * @param connection konekcija tipa Connection
+	 */
+	public synchronized void push(Connection connection) {
+		connectionPool.add(connection);
+	}
 
-    /**
-     * Implementacija queue metode pop.
-     * 
-     * Vraca se konekcija sa pocetka liste. Ukoliko je lista prazna baca se izuzetak.
-     * 
-     * @return connection konekcija tipa Connection
-     * @throws Exception ukoliko je lista prazna
-     */
-    public synchronized Connection pop() throws Exception {
-        if (connectionPool.isEmpty()) {
-            throw new Exception("Nema slobodne konekcije");
-        }
-        Connection connection = connectionPool.get(0);
-        connectionPool.remove(0);
-        return connection;
-    }
+	/**
+	 * Implementacija queue metode pop.
+	 * 
+	 * Vraca se konekcija sa pocetka liste. Ukoliko je lista prazna baca se
+	 * izuzetak.
+	 * 
+	 * @return connection konekcija tipa Connection
+	 * @throws Exception ukoliko je lista prazna
+	 */
+	public synchronized Connection pop() throws Exception {
+		if (connectionPool.isEmpty()) {
+			throw new Exception("Nema slobodne konekcije");
+		}
+		Connection connection = connectionPool.get(0);
+		connectionPool.remove(0);
+		return connection;
+	}
+
+	private Connection getConnection() throws Exception {
+
+		Connection connection;
+
+		try (FileReader fr = new FileReader("./src/main/java/resources/dbconfig.json")) {
+			Gson gson = new Gson();
+
+			JsonObject jsonObject = gson.fromJson(fr, JsonObject.class);
+
+			String url = jsonObject.get("url").getAsString();
+			String username = jsonObject.get("username").getAsString();
+			String password = jsonObject.get("password").getAsString();
+
+			connection = DriverManager.getConnection(url, username, password);
+			System.out.println("Konekcija sa bazom podataka uspesno uspostavljena!");
+			connection.setAutoCommit(false);
+		} catch (IOException e) {
+			e.printStackTrace();
+			throw e;
+		}
+		return connection;
+	}
+
 }
